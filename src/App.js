06f71851941dbf7ac8fc5ee29e64629043401491
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-10 22:25:10 +0300

    componenets and scss files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Header } from './components/Header';
+import { Doctors } from './components/Doctors';
 
 function App() {
   return (
+
     <div className="App">
-
-    <h1>Hello React</h1>
-
+      <Header/>
+      <Doctors/>
     </div>
+
+
   );
 }
 
---
+++ src/components/Consulted.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Consulted = () => {
+  return (
+    <div>Consulted</div>
+  )
+}
