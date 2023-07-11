6dd7764ecbbb0a7addb382df55471a2a024d985a
Koray KOKSAL <tkkoksal@icloud.com>
2023-07-11 21:57:15 +0300

    modal created

--- src/components/DoctorCards.jsx
+++ src/components/DoctorCards.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import "../sass/Doctors.scss"
+import { Modal } from 'react-bootstrap'
+import { useState } from 'react'
+
 export const DoctorCards = ({id,name,dep,img}) => {
 
+    const [show, setShow] = useState(false)
+    
+    const handleShowModal=()=>{
+        setShow(true)
+    }
 
-    const handleClick=()=>{
-
+    const handleCloseModal=()=>{
+        setShow(false)
     }
 
   return (
-    <div onClick={handleClick}>
 
-        <div class="card drCards text-center m-auto">
-            <img src={img} alt={name} />
-        <div class="card-body">
-            <h5 class="card-title">{name}</h5>
-            <p class="card-text">{dep}</p>
-        </div>
+    <>
+   
+        <div onClick={handleShowModal}>
+
+            <div class="card drCards text-center m-auto">
+                <img src={img} alt={name} />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">{dep}</p>
+                </div>
+            </div>
         </div>
 
-    </div>
+        <Modal show={show} setShow={setShow} className='mdl'>
+            <Modal.Body>
+                <div className='row text-center m-auto'>
+                    <h4>{name}</h4>
+                    <img src={img} alt={name} />
+                    <input type="text" placeholder='Name Surname' />
+                    <input type="datetime-local" />
+                </div>
+
+
+            </Modal.Body>
+            <Modal.Footer>
+                <button onClick={handleCloseModal} className='btn btn-secondary' type='submit'>Close</button>
+                <button className='btn btn-primary' type='submit'>Submit</button>
+            </Modal.Footer>
+        </Modal>
+    </>
+
   )
 }
